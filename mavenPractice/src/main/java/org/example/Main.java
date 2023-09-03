0582bd63cfc1fe299319f8227f35ba1aa7cd4dfe
Saravanan <saravanasaro2410@gmail.com>
2023-09-03 16:39:42 +0530

    Updated the File!

--- mavenPractice/src/main/java/org/example/Main.java
+++ mavenPractice/src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,34 +9,31 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class Main {
-
-    // TODO: Saravanan
-
     public static void main(String[] args) throws IOException {
-            String apiUrl = "https://api.nationalize.io/?name=nathaniel";
+        String apiUrl = "https://api.nationalize.io/?name=nathaniel";
 
-            URL url = new URL(apiUrl);
+        URL url = new URL(apiUrl);
 
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-            connection.setRequestMethod("GET");
+        connection.setRequestMethod("GET");
 
-            int responseCode = connection.getResponseCode();
+        int responseCode = connection.getResponseCode();
 
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                StringBuilder response = new StringBuilder();
-                String line;
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            String line;
 
-                while ((line = reader.readLine()) != null) {
-                    response.append(line);
-                }
-                reader.close();
-
-                JSONObject jsonResponse = new JSONObject(response.toString());
-                System.out.println(jsonResponse);
-            } else {
-                System.out.println("HTTP Request Failed with response code: " + responseCode);
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
             }
+            reader.close();
+
+            JSONObject jsonResponse = new JSONObject(response.toString());
+            System.out.println(jsonResponse);
+        } else {
+            System.out.println("HTTP Request Failed with response code: " + responseCode);
+        }
     }
 }
