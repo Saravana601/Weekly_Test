1877966d4e06a60b10626744453bbc212c9eab62
Saravanan <saravanasaro2410@gmail.com>
2023-09-03 16:17:10 +0530

    Added the Api-Calling-Using-Java File

---
+++ mavenPractice/src/main/java/org/example/Main.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Main {
+
+    // TODO: Saravanan
+
+    public static void main(String[] args) throws IOException {
+            String apiUrl = "https://api.nationalize.io/?name=nathaniel";
+
+            URL url = new URL(apiUrl);
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder response = new StringBuilder();
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                reader.close();
+
+                JSONObject jsonResponse = new JSONObject(response.toString());
+                System.out.println(jsonResponse);
+            } else {
+                System.out.println("HTTP Request Failed with response code: " + responseCode);
+            }
+    }
+}
